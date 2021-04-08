83e03c6aa985c8c2e2a84527e3458da30107de29
jaspermarsman <jaspermarsman@gmail.com>
2021-04-08 13:55:33 +0200

    alle opdrachten gedaan

--- functions.js
+++ functions.js
@@ -15,19 +15,49 @@ console.log(greeting);
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
 
+function getSchool() {
+  return "Novi Hogeschool";
+}
+
+const school = getSchool();
+
+console.log(school);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function getCompliment(){
+  return "Lekker bezig met die functies, Jan!";
+}
 
+const compliment = getCompliment();
+
+console.log(compliment);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades(){
+  return [8, 8.5, 6, 7];
+}
 
+const allGrades = getGrades();
+
+console.log(allGrades);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails(){
+  const person = {
+    firstName: "Jasper",
+        lastName: "Marsman",
+  }
+  return person;
+}
+
+const wieIsDit = getDetails();
+console.log(wieIsDit);
